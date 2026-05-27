097f2863539eb12d20c68f21f645f00a9268dad3
charlesarcharles98-del <charlesarcharles98@gmail.com>
2026-05-27 17:11:47 +0000

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,44 @@
+public class hello{
+    public static void main(String[]agrs) {
+    
+        System.out.println("bonjour a vous ");
+
+        int age = 16;
+        String nom = "Alex";
+        double taille = 1.75;
+        if(age>=18)
+            System.out.println("tu es majeur");
+            else
+                System.out.println("tu es mineur");
+
+        System.out.println(age);
+        System.out.println(nom);
+        System.out.println(taille);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
